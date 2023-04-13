33d0f56a69e00354cf3e48fd82f7833d069d562d
vasylenkodi <vasylenko1daniil@gmail.com>
2023-04-13 16:43:14 +0200

    добавляет решение задания 2

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types';
 import data from '../../data.json';
+import css from './Statistics.module.css';
 import { Stat } from './stat/Stat';
 
 export const Statistics = () => {
   return (
-    <section className="statistics">
-      <h2 className="title">Upload stats</h2>
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
 
-      <ul className="stat-list">
-              {data.map(dataItem => {
-          return (<Stat
-            key={dataItem.id}
-            label={dataItem.label}
-            percentage={dataItem.percentage}
-          />);
+      <ul className={css.statList}>
+        {data.map(dataItem => {
+          return (
+            <Stat
+              key={dataItem.id}
+              label={dataItem.label}
+              percentage={dataItem.percentage}
+            />
+          );
         })}
       </ul>
     </section>
--- src/components/statistics/stat/Stat.jsx
+++ src/components/statistics/stat/Stat.jsx
@@ -1,10 +1,17 @@
 import css from './Stat.module.css';
 
 export const Stat = ({ label, percentage }) => {
+  const bgcColorOfStat = {backgroundColor: getRandomHexColor()};
   return (
-    <li className={css.item}>
+    <li className={css.item} style={bgcColorOfStat}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
     </li>
   );
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
